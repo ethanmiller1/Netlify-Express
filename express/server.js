48216e52a695173b3f7cd1f22cbbad459c501b91
Ethan Miller <ethan.romans5.8@gmail.com>
2019-06-29 17:22:33 -0500

    route home to netlify directory

--- express/server.js
+++ express/server.js
@@ -5,22 +5,15 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
+
+// Home route.
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  res.render('index');
+  res.send('Hello World!')  
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-
-// Home route.
-app.get('/', (req, res) => {
-    res.render('index');
-    res.send('Hello World!')  
-});
 
 module.exports = app;
 module.exports.handler = serverless(app);
